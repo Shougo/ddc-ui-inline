aea344065857ad159974f723ee230f1ba3b1564c
Shougo Matsushita <Shougo.Matsu@gmail.com>
2022-10-23 12:04:40 +0900

    Add UI files

---
+++ denops/@ddc-uis/inline.ts
@@ -0,0 +1,36 @@
+import { Context, DdcItem } from "https://deno.land/x/ddc_vim@v3.0.0/types.ts";
+import { BaseUi } from "https://deno.land/x/ddc_vim@v3.0.0/base/ui.ts";
+import { Denops } from "https://deno.land/x/ddc_vim@v3.0.0/deps.ts";
+
+export type Params = {
+  highlight: string;
+};
+
+export class Ui extends BaseUi<Params> {
+  async show(args: {
+    denops: Denops;
+    context: Context;
+    completePos: number;
+    items: DdcItem[];
+    uiParams: Params;
+  }): Promise<void> {
+    await args.denops.call(
+      "ddc#ui#inline#_show",
+      args.completePos,
+      args.items,
+      args.uiParams.highlight,
+    );
+  }
+
+  async hide(args: {
+    denops: Denops;
+  }): Promise<void> {
+    await args.denops.call("ddc#ui#inline#_hide");
+  }
+
+  params(): Params {
+    return {
+      highlight: "Comment",
+    };
+  }
+}
